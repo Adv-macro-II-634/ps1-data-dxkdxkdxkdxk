209cce58e9c03ef83d0bf692c989eaf95245a0bc
XiangkunDai <xiangkunbendai@gmail.com>
2019-09-04 19:41:49 -0400

    Update PS1.R
    income dataset

--- PS1.R
+++ PS1.R
@@ -4,3 +4,7 @@ data = readxl::read_xlsx('SCFP2007.xlsx')
 y = data.frame(data$YY1,data$Y1,data$WGT,data$WAGEINC,
       data$INCOME,data$INCCAT,data$BUSSEFARMINC,data$INTDIVINC,
       data$SSRETINC,data$KGINC)  ## income
+
+y[,4:10] = y[,4:10]*0.88  # converting 2016 USD to 2007 USD
+
+y$earining = y$data.INCOME +  0.863*y$data.BUSSEFARMINC # constructing earning
