ae859cafecd158359cf40729f4b6605be3931638
XiangkunDai <xiangkunbendai@gmail.com>
2019-09-04 20:44:50 -0400

    earings quantiles

--- PS1.R
+++ PS1.R
@@ -1,5 +1,7 @@
-setwd("~/Desktop/advanced macro")
+setwd("~/Documents/GitHub/ps1-data-dxkdxkdxkdxk")
 data = readxl::read_xlsx('SCFP2007.xlsx')
+install.packages('reldist')
+library(reldist)
 
 y = data.frame(data$YY1,data$Y1,data$WGT,data$WAGEINC,
       data$INCOME,data$INCCAT,data$BUSSEFARMINC,data$INTDIVINC,
@@ -7,4 +9,10 @@ y = data.frame(data$YY1,data$Y1,data$WGT,data$WAGEINC,
 
 y[,4:10] = y[,4:10]*0.88  # converting 2016 USD to 2007 USD
 
-y$earining = y$data.INCOME +  0.863*y$data.BUSSEFARMINC # constructing earning
+y$earining = y$data.WAGEINC + 0.863*y$data.BUSSEFARMINC # constructing earning
+e_quantiles = data.frame(wtd.quantile(y$earining/1000,
+                                  q=c(0,0.01,0.05,0.1,0.2,0.4,0.6,0.8,0.9,0.95,0.99,1),
+                                  weight = y$data.WGT))
+names(e_quantiles)[1] = "quantiles"
+e_quantiles$quantiles = round(e_quantiles$quantiles,digits=2) ### earings quantile table 1
+
